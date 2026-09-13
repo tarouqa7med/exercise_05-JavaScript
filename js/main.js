a256cf2d7d30b91c3632e3f97419730e8b524a6b
Tarouq Ahmed <tariq.ahmed67006@gmail.com>
2026-09-13 21:04:47 +0300

    kkk

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 
-let firstName, lastName, email, age, phone, option;
+let firstName, lastName, email, age, phone, option, submitBtn;
+
+firstName = document.querySelector('input[id="firstName"]');
+lastName = document.querySelector('input[id="lastName"]');
+email = document.querySelector('input[id="email"]');
+age = document.querySelector('input[id="age"]');
+mobileNumber = document.querySelector('input[id="mobileNumber"]');
+submitBtn = document.querySelector(".submitBtn");
+
+console.log(firstName)
+console.log(lastName)
+console.log(email)
+console.log(age)
+console.log(mobileNumber)
+console.log(submitBtn)
